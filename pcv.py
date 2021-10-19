52a4e8958647c6960fce2452d62e99693a773bd6
Gabriel de Melo Marcondes <33481928+GMMGabriel@users.noreply.github.com>
2021-10-19 09:07:57 -0300

    update 1

--- pcv.py
+++ pcv.py
@@ -1,11 +1,11 @@
 # IMPORTS ============================================================================================================================
 
 import matplotlib.pyplot as plt
-import numpy as np
 import random
 import math
 import itertools as tls
 from datetime import datetime
+import time
 import os
 
 
@@ -23,6 +23,8 @@ def gen_cordinate(n):
     y = list(range(n))
     random.shuffle(x)
     random.shuffle(y)
+    x = [float(round(value + (random.uniform(-0.3, 0.3)), 1)) for value in x]
+    y = [float(round(value + (random.uniform(-0.3, 0.3)), 1)) for value in y]
     return tuple(x), tuple(y)
 
 
@@ -38,16 +40,17 @@ def distance_between_two_cities(p1, p2):
     return round(math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2), 3)
 
 
-def calc_distances_between_cities(x, y):
+def calc_distances_between_cities(x, y, n):
     '''
     x: tupla com os valores do eixo x.
     y: tupla com os valores do eixo y.
+    n: número de cidades.
     ---
     Gera uma matriz com os valores referente as distancias entre cada cidade.
     ---
     Retorna essa matriz.
     '''
-    return np.array([list(float(distance_between_two_cities((x[i], y[i]), (x[j], y[j]))) for i in range(n)) for j in range(n)])
+    return tuple([tuple(float(distance_between_two_cities((x[i], y[i]), (x[j], y[j]))) for i in range(n)) for j in range(n)])
 
 
 def gen_possible_paths(n, start_city):
@@ -59,8 +62,8 @@ def gen_possible_paths(n, start_city):
     ---
     Retorna a tupla com os resultados.
     '''
-    index_cities_to_str = tuple([str(x) for x in range(n) if x != start_city])
-    return tuple(tls.permutations(index_cities_to_str))
+    index_cities = tuple([int(x) for x in range(n) if x != start_city])
+    return tuple(tls.permutations(index_cities))
 
 
 def calc_total_distances_from_the_paths(start_city, matrix_distances_between_cities, possible_paths):
@@ -75,12 +78,14 @@ def calc_total_distances_from_the_paths(start_city, matrix_distances_between_cit
     '''
     total_distances_from_the_paths = [0]*len(possible_paths)
 
-    for i in range(len(possible_paths)): # percorre a tupla para poder chegar nas tuplas com os caminhos.
+    # percorre a tupla para poder chegar nas tuplas com os caminhos.
+    for i in range(len(possible_paths)):
         # Já de início, soma a distancia entre a cidade inicial (start_city) e a segunda cidade do percurso, com a distancia
         # entre a penúltima cidade do percurso e a cidade final (start_city).
         total = (matrix_distances_between_cities[start_city][int(possible_paths[i][0])]) + (matrix_distances_between_cities[start_city][int(possible_paths[i][-1])])
 
-        for j in range(len(possible_paths[0])-1): # percorre a tupla que contem os caminhos.
+        # percorre a tupla que contem os caminhos.
+        for j in range(len(possible_paths[0])-1):
             # Soma ao valor, a distancia entre as cidades do caminho possível.
             total += matrix_distances_between_cities[int(possible_paths[i][j])][int(possible_paths[i][j+1])]
 
@@ -99,11 +104,15 @@ def start(n, start_city):
     ---
     Retorna None.
     '''
-    x, y = gen_cordinate(n) # gera as coordenadas x e y
+    ini = time.time()
 
-    matrix_distances_between_cities = calc_distances_between_cities(x, y) # calcula a distancia entre cada cidade
+    x, y = gen_cordinate(n)  # gera as coordenadas x e y
 
-    possible_paths = gen_possible_paths(n, start_city) # gera todos os caminhos possíveis
+    matrix_distances_between_cities = calc_distances_between_cities(x, y, n)  # calcula a distancia entre cada cidade
+
+    # gera todos os caminhos possíveis
+    possible_paths = gen_possible_paths(n, start_city)
+    possible_paths = possible_paths[:int((len(possible_paths) - len(possible_paths)/(n-1)))]
 
     # Calcula a distancia total de cada caminho possível.
     total_distances_from_the_paths = calc_total_distances_from_the_paths(start_city, matrix_distances_between_cities, possible_paths)
@@ -112,14 +121,16 @@ def start(n, start_city):
     value_min_index = total_distances_from_the_paths.index(min(total_distances_from_the_paths))
 
     # Armazena em uma tupla os índices das cidades na ordem que fazem o menor percurso.
-    way_made = (start_city,) + tuple([int(value) for value in possible_paths[value_min_index]]) + (start_city,)
+    way_made = (start_city,) + tuple([int(value)for value in possible_paths[value_min_index]]) + (start_city,)
     # Pega o valor da distancia total percorrida.
     total_distance_from_the_way_made = total_distances_from_the_paths[value_min_index]
 
     # Organiza as coordenadas na ordem do menor percurso.
-    _x = [int(x[value]) for value in way_made]
-    _y = [int(y[value]) for value in way_made]
-    
+    x = tuple([float(x[value]) for value in way_made])
+    y = tuple([float(y[value]) for value in way_made])
+
+    plt.figure(figsize=(7, 7))
+
     # Variáveis auxiliares para colocar os caminhos entre cada cidade.
     x_aux = 0
     y_aux = 0
@@ -128,41 +139,62 @@ def start(n, start_city):
     for i in range(n):
         for j in range(i, n):
             if i != j:
-                x_aux = list(str(_x[i])) + list(str(_x[j]))
-                x_aux = [int(value) for value in x_aux]
-                y_aux = list(str(_y[i])) + list(str(_y[j]))
-                y_aux = [int(value) for value in y_aux]
+                x_aux = (float(x[i]), float(x[j]))
+                x_aux = [float(value) for value in x_aux]
+                y_aux = (float(y[i]), float(y[j]))
+                y_aux = [float(value) for value in y_aux]
                 plt.plot(x_aux, y_aux, 'lightgray')
 
     # Coloca o último caminho novamente, mas agora com um "label" para aparecer na legenda.
     plt.plot(x_aux, y_aux, 'lightgray', label="Conexões entre cidades")
 
     # Coloca o Caminho percorrido.
-    plt.plot(_x, _y, 'o-', color="green", label="Caminho percorrido")
+    plt.plot(x, y, 'o-', color="green", label="Caminho percorrido")
+    for i in range(len(x)-1):
+        plt.text(((x[i]+x[i+1])/2), # x
+                 ((y[i]+y[i+1])/2), # y
+                 str(matrix_distances_between_cities[i][(i+1) % len(matrix_distances_between_cities)]), # texto
+                 fontsize=10, # tamanho da fonte
+                 horizontalalignment="center", # alinhamento horizontal
+                 verticalalignment="center", # alinhamento vertical
+                 fontweight="bold", # negrito
+                 backgroundcolor=(1, 1, 1, .5)) # cor de fundo do texto
 
     # Coloca os índices de cada cidade em cima delas.
-    for i in range(len(_x)-1):
-        plt.text(_x[i], _y[i] + 0.2, str(way_made[i]), color="k", fontsize=10, fontweight="bold", horizontalalignment="center")
+    for i in range(len(x)-1):
+        plt.text(x[i], y[i] + 0.2, str(way_made[i]), color="k", fontsize=12, fontweight="bold", horizontalalignment="center")
 
-    way_made = str(way_made)
-    
     # Coloca a cidade inicial e final de uma cor diferente.
-    plt.plot(_x[0], _y[0], "o", color="#591ff9", label="Ponto de partida e chegada")
+    plt.plot(x[0], y[0], "o", color="#591ff9",
+             label="Ponto de partida e chegada")
 
     # Demais configurações para a janela do gráfico.
-    plt.legend(loc=3) # legenda
-    plt.axis([-0.5, n-0.5, -3, n+1]) # limita a área de exibição
+    plt.legend(loc=3)  # legenda
+    min_axis_x = -0.4
+    max_axis_x = n-0.6
+    min_axis_y = -3  # para caber a legenda em baixo
+    # para caber o texto do caminho feito e o texto da distancia total percorrida em cima
+    max_axis_y = n+1
+    # limita a área de exibição
+    plt.axis([min_axis_x, max_axis_x, min_axis_y, max_axis_y])
     plt.xlabel("Eixo X")
     plt.ylabel("Eixo Y")
     plt.title("PCV (Problema do Caixeiro Viajante) com " + str(n) + " cidades")
-    plt.text(-0.4, n+0.5, "Caminho percorrido: " + way_made, fontsize=10) # coloca o texto que mostra o caminho percorrido
-    plt.text(-0.4, n, "Distancia total do percurso: " + str(round(total_distance_from_the_way_made, 3)), fontsize=10)
+    # coloca o texto que mostra o caminho percorrido
+    plt.text(min_axis_x + 0.1, max_axis_y-0.5, "Caminho percorrido: " + str(way_made), fontsize=10)
+    # coloca o texto que mostra a distancia total percorrida
+    plt.text(min_axis_x + 0.1, max_axis_y-1.1, "Distancia total do percurso: " + str(round(total_distance_from_the_way_made, 3)), fontsize=10)
+
+    fim = time.time() - ini
+    print(fim)
+
     plt.show()
 
 
 # MAIN ===============================================================================================================================
 
 if __name__ == '__main__':
+
     os.system('cls')
 
     now = datetime.now()
@@ -189,7 +221,11 @@ if __name__ == '__main__':
         i = input("Índice da cidade para iniciar (responda em branco para ser aleatório): ")
         try:
             i = i_aux = int(i)
-            break
+            if i < 0 or i >= n:
+                print("\n\tPor favor, informe um número entre 0 e " + str(n - 1) + ".\n")
+                i_aux = -1
+            else:
+                break
         except ValueError as e:
             if i == '':
                 i = random.randint(0, n-1)
@@ -203,3 +239,7 @@ if __name__ == '__main__':
     print("Índice da cidade inicial:", (i if i_aux > -1 else "aleatório"))
 
     start(n, i)
+
+    # for i in range(4, 9):
+    #     %time start(i, 0)
+    #     print()
